44ac426de301957c4fadb47ae385be79b8fafda4
griffinGC <griffinDouble@gmail.com>
2018-11-15 10:34:26 +0900

    test_change

--- src/components/mainpage/Mainpage.vue
+++ src/components/mainpage/Mainpage.vue
@@ -5,60 +5,76 @@
     <i class="xi-home-o"></i>
     <span>도시</span><span>in</span><span>out</span> <i class="xi-plus-circle-o" v-on:click="addCity"></i>
         
-    <div class="putCityInfo" v-for="city in cityInfo" v-bind:key="city.num" v-bind="city">
+    <div class="putDot" v-for="city in dot" v-bind:key="city.num" v-bind="city">
         <form class="putCity">
             <i class="xi-check-circle"></i>
             <input class="mainCity" type="text" placeholder="시작 도시" v-model="city.mainCity"/>
             <input class="inCity" type="text" placeholder="in" v-model="city.inDay"/>
             <input class="outCity" type="text" placeholder="out" v-model="city.outDay"/>
             <i class="xi-plus-square" v-on:click="addCheckList"></i>            
-            <!-- <div class="checkList" v-for="checklist in checkList" v-bind="checklist" v-bind:key="checklist.index" v-show="isCheck"> -->
-            <div class="checkList" v-for="city in cityInfo" v-bind="checklist" v-bind:key="city.index" v-show="isCheck">   
-                <input type="checkbox" class="checklist">
-                <input type="text" class="checkMain">
-            </div>
-        </form>
-    </div>    
-    <h1>{{cityInfo}}</h1> 
+            <ul class="list-item" v-show="isCheck">
 
+            </ul>
+        </form>
+    </div>
+    <div v-for="action in dot" v-bind:key="action.index">
+        <h1> {{action.checkLists[0].title}}</h1>
+    </div>
   </div>
 </template>
 
 <script>
-
+import CheckList from '@/components/mainpage/checkList.vue'
 
 export default {
   
   name: 'MainPage',
-  components: {
 
+  components: {
+      CheckList
   },
+
   methods:{
       addCity(){
-          this.cityInfo.push({
+          this.dot.push({
             num : ++this.num,
             mainCity : '',
             inDay : '',
             outDay : '',
-            // checkList :[]
+            checkLists :{
+                id : '',
+                title : '',
+                actions : ''
+            }
           })
-          this.cityInfo.mainCity = "시작도시";
-          this.cityInfo.inDay ="in";
-          this.cityInfo.outDay="out"
+          this.dot.mainCity = "시작도시";
+          this.dot.inDay ="in";
+          this.dot.outDay="out"
       },
       addCheckList(){
           this.isCheck = true;
+          this.dot.checkLists.push({
+                id : '',
+                title : '',
+                actions : ''
+          })
       }
 
   },
   data(){
     return {
-        cityInfo:[
+        dot:[
             {
-            mainCity : '',
-            inDay : '',
-            outDay : '',
-            checkList:[]
+                mainCity : 'seoul',
+                inDay : '',
+                outDay : '',
+                checkLists:[
+                        {
+                    id : '',
+                    title : 'tllll',
+                    actions : 'do something'
+                        }
+                    ]
             }            
         ],
         num : 0,
---
+++ src/components/mainpage/checkList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="checklist">
+    <li>
+        <input type="checkbox" class="checklist">                    
+        <input type="text" class="checkMain" ><br>
+        <input type="textarea" cols="50" rows="20" class="action">
+    </li> 
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  
+  name: 'CheckList',
+  components: {
+
+  },
+  methods:{
+      addCheckList(){
+          this.isCheck = true;
+          this.dot.checkLists.push({
+                id : '',
+                title : '',
+                actions : ''
+          })
+      }
+
+  },
+  
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+
+
+<style>
+span{
+    padding : 3%;
+}
+.mainCity{
+    width: 20%
+}
+.inCity , .outCity{
+    width : 5%
+}
+
+i{
+    padding: 20px;
+}
+
+</style>
+
