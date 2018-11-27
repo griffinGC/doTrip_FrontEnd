663e1cc5f6da44e43902ee1aae4a845f50cfccc4
griffinGC <griffinDouble@gmail.com>
2018-11-27 19:38:34 +0900

    modify_schedule

--- src/components/calendar/Schedule.vue
+++ src/components/calendar/Schedule.vue
@@ -5,24 +5,46 @@
             <h4>{{city.inDay}} - {{city.outDay}} </h4>
         </div><br>
         
-        <section class="panel">	
-            <input type="checkbox" id="mark-all" @click="selectAll" :checked="areAllSelected">
+        <section class="panel mx-5" >	
+            <b-form-checkbox id="mark-all"></b-form-checkbox>
             <input v-model="newTask" @keyup.enter="addTask" placeholder="일정을 추가해 주세용!" autofocus class="text-input">
-            <button @click="clearList">일정추가</button>
+            <b-button :variant="primary"  @click="addTask">일정추가</b-button><br>
+            <b-form-textarea  id="doAction" v-model="doAction" rows="6" placeholder="할일을 구체적으로 적어주세요!"></b-form-textarea>
+
         </section>
         <div v-for="city in dot" v-bind:key="city.num" class="list">
             <div v-for="todo in city.checkList" v-bind:key="todo.id">
-                <li>{{todo.title}}</li><br>
-                <h6> {{todo.action}} </h6><br><br>
+                <div class="row">
+                    <div class="col text-center my-3">
+                        <li class=todoTitle>{{todo.title}}</li>
+                    </div>
+                    <div class="col float-left">
+                    <b-button @click="showTask">▼</b-button>
+                    <b-button @click="hideTask">▲</b-button>
+                    </div>
+                </div>
+                <h6 v-show="okAction" v-bind="todo.okAction"> {{todo.action}} </h6><br><br>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+
 export default {
     name: 'Schedule',
     methods:{
+        addTask(){
+            this.axois.post()
+        },
+        showTask(){
+            this.okAction = true;
+        },
+        hideTask(){
+            this.okAction = false;
+        }
 
     },
     data(){
@@ -45,7 +67,8 @@ export default {
                         action : "비치웨어 챙겨가기! 중요한건 숙소에 두자 꼭꼭"
                     }]
                 }
-            ]
+            ],
+            okAction : false
             
         }
    },
@@ -53,6 +76,12 @@ export default {
 </script>
 
 <style>
+#doAction{
+    
+}
+.todoTitle{
+    font-size: 20px;
+}
 .schedule{
   text-align: center;
   margin-top: 60px;
