5eafd993cabeab8b9627543241664e8e8a035d17
blcocas <l02220hj@gmail.com>
2018-12-06 14:09:08 +0900

    localhost change

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://13.209.77.95:3000/',
+        target: 'http://localhost:3000/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
